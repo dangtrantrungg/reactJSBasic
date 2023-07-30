0e4228e6edadf67b81ee61e64b2912166cce983b
Dang Tran Trung <dangtrantrung0501@gmail.com>
2023-07-31 00:18:37 +0700

    accquaint statless/statefull

--- src/components/Example/ChildComponent.js
+++ src/components/Example/ChildComponent.js
@@ -1,26 +1,46 @@
 import React from 'react';
 
 
-class ChildComponent extends React.Component {
-    render() {
-        let { name, age, job } = this.props
-        return (
-            <>
-                {console.log(">>>>>>>> check props", this.props)}
-                <div>this is ChildComponent {name} - {age} </div>
-                <div>
-                    {job.map((item, index) => {
-                        return (
-                            <div key={item.id}>
-                                {item.nameJob} - {item.salary}
-                            </div>
-                        )
-                    })}
-                </div>
-            </>
-        )
-    }
+// class ChildComponent extends React.Component {
+//     render() {
+//         let { name, age, job } = this.props
+//         return (
+//             <>
+//                 {console.log(">>>>>>>> check props", this.props)}
+//                 <div>this is ChildComponent {name} - {age} </div>
+//                 <div>
+//                     {job.map((item, index) => {
+//                         return (
+//                             <div key={item.id}>
+//                                 {item.nameJob} - {item.salary}
+//                             </div>
+//                         )
+//                     })}
+//                 </div>
+//             </>
+//         )
+//     }
+// }
+
+function ChildComponent(props) {
+    let { name, age, job } = props
+    return (
+        <>
+            {console.log(">>>>>>>> check props", props)}
+            <div>this is ChildComponent {name} - {age} </div>
+            <div>
+                {job.map((item, index) => {
+                    return (
+                        <div key={item.id}>
+                            {item.nameJob} - {item.salary}
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    )
 }
+
 
 export default ChildComponent;
 
