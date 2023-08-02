7ae1381104b51e426f957b6dd72787319c16778d
Dang Tran Trung <dangtrantrung0501@gmail.com>
2023-08-03 03:19:51 +0700

    conditional rendering

--- src/components/Example/ChildComponent.js
+++ src/components/Example/ChildComponent.js
@@ -1,45 +1,80 @@
 import React from 'react';
 
 
-// class ChildComponent extends React.Component {
-//     render() {
-//         let { name, age, job } = this.props
-//         return (
-//             <>
-//                 {console.log(">>>>>>>> check props", this.props)}
-//                 <div>this is ChildComponent {name} - {age} </div>
-//                 <div>
-//                     {job.map((item, index) => {
-//                         return (
-//                             <div key={item.id}>
-//                                 {item.nameJob} - {item.salary}
-//                             </div>
-//                         )
-//                     })}
-//                 </div>
-//             </>
-//         )
-//     }
-// }
+class ChildComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            valid: false
 
-function ChildComponent(props) {
-    let { name, age, job } = props
-    return (
-        <>
-            {console.log(">>>>>>>> check props", props)}
-            <div>this is ChildComponent {name} - {age} </div>
-            <div>
-                {job.map((item, index) => {
-                    return (
-                        <div key={item.id}>
-                            {item.nameJob} - {item.salary}
+        };
+    }
+
+    //biến đổi trạng thái của valid sau khi click
+    handleShowHide = () => {
+        this.setState({
+            valid: !this.state.valid
+        })
+    }
+    render() {
+        let { name, age, job } = this.props
+        const { valid } = this.state
+        return (
+            <>
+                {console.log(">>>>>>>> check props", this.props)}
+                <div>this is ChildComponent {name} - {age} </div>
+                {/**
+                 * kiểm tra xem trạng thái của valid là gì
+                 * nếu trạng thái là false thì hiện nút show
+                 * nếu trạng thái là true thì hiện nút hide
+                 * so sánh được viết dưới dạng Inline If-Else with Conditional Operator
+                */}
+                {valid === false ?
+                    <div>
+                        <button onClick={() => this.handleShowHide()}>Show</button>
+                    </div>
+                    :
+                    // muốn render nhiều <dive> thì cần phải sử dụng JSX fragment <>.....</>
+                    <>
+                        <div className='job-list'>
+                            {job.map((item, index) => {
+                                return (
+                                    <div key={item.id}>
+                                        {item.nameJob} - {item.salary}
+                                    </div>
+                                )
+                            })}
                         </div>
-                    )
-                })}
-            </div>
-        </>
-    )
+
+                        <div><button onClick={() => this.handleShowHide()}> Hide</button></div >
+                    </>
+                }
+
+
+
+            </>
+        )
+    }
 }
+
+// function ChildComponent(props) {
+//     let { name, age, job } = props
+//     return (
+//         <>
+//             {console.log(">>>>>>>> check props", props)}
+//             <div>this is ChildComponent {name} - {age} </div>
+//             <div>
+//                 {job.map((item, index) => {
+//                     return (
+//                         <div key={item.id}>
+//                             {item.nameJob} - {item.salary}
+//                         </div>
+//                     )
+//                 })}
+//             </div>
+//         </>
+//     )
+// }
 
 
 export default ChildComponent;
