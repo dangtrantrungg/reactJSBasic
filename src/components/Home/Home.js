75ab07b646c14f13663f4dfdb95bb0df231b212e
Dang Tran Trung <dangtrantrung0501@gmail.com>
2023-09-25 10:54:37 +0700

    mapping state redux to props reactjs

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
 import logo from "../../assets/img/meme.png"
+import { connect } from "react-redux";
 
 class Home extends React.Component {
     componentDidMount() {
@@ -10,14 +11,19 @@ class Home extends React.Component {
     }
 
     render() {
+        console.log(">>>>>> check props", this.props.dataRedux)
         return (
             <>
                 <div>Hello World</div>
                 <div>
-                    <img src={logo} />
+                    <img src={logo} alt="" />
                 </div>
             </>
         )
     }
 }
-export default withRouter(Home);
+
+const mapStateToProps = (state) => {
+    return { dataRedux: state.users }
+}
+export default connect(mapStateToProps)(withRouter(Home));
