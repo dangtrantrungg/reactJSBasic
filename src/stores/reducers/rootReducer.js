48688ec61cb551c6b27e3ae9df60414d348330f7
Dang Tran Trung <dangtrantrung0501@gmail.com>
2023-09-25 10:05:39 +0700

    initialize the redux

---
+++ src/stores/reducers/rootReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+    users: [
+        { id: 1, name: "Trung" },
+        { id: 2, name: "hello" }
+    ]
+}
+
+
+const rootReducer = (state = initState, action) => {
+    <div> Hello World</div>
+    return state;
+}
+export default rootReducer
